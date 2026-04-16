976de7017f0967dd686072cd74d0a8433bdbb4c1
FO7S <faisal.ut1@gmail.com>
2026-04-16 20:58:39 +0300

    use env API URL

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import bagIcon from './assets/bag.svg';
 // ================================================================
 // API
 // ================================================================
-const API = "http://localhost:8000";
+const API = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 async function sendOTP(email) {
   const form = new FormData();
